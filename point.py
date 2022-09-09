ac0418e23ebf71d0171d6e3ec103297f9c148de3
Pavlov83 <resolversoft@gmail.com>
2022-09-10 00:41:40 +0300

    initialize and sub/ add point

--- point.py
+++ point.py
@@ -8,3 +8,8 @@ class Point:
             self.x + other.x,
             self.y + other.y
         )
+    def __sub__(self,other):
+        return Point(
+            self.x - other.x,
+            self.y - other.y
+        )
